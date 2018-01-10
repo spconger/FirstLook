e79922d77d6b5371dbbe92c4b7bacd8c1e1c36f6
Steve Conger <steve@spconger.com>
2018-01-10 11:56:24 -0800

    first commit

---
+++ Models/Mailing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstLook.Models
+{
+    public class Mailing
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
